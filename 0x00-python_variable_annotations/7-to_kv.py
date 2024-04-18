8acfe4c98cba90ab8b2a53e11ab8bd86294d97c8
walaeddine01 <walaaeddine33@gmail.com>
2024-04-18 19:47:50 +0200

    I made the changes on 2024-04-18 19:47:50

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""
+This module contais the sum_mixed_list method using typing annotaions
+"""
+from typing import Tuple
+
+
+def to_kv(k: str, v: float | int) -> Tuple:
+    """
+    """
+    return (k, float((v * v)))
