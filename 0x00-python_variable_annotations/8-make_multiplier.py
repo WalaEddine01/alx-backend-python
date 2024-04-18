87f050b4909a4c5b6a2d3e0830f774d721dfc2fb
walaeddine01 <walaaeddine33@gmail.com>
2024-04-18 20:09:54 +0200

    I made the changes on 2024-04-18 20:09:54

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This module contais the sum_mixed_list method using typing annotaions
+This module contais the make_multiplier  method using typing annotaions
 """
 from typing import Callable
 
