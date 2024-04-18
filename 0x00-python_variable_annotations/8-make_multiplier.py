e2c7fdce24b23ec1f44bb506062e190eccd3956c
walaeddine01 <walaaeddine33@gmail.com>
2024-04-18 19:54:00 +0200

    I made the changes on 2024-04-18 19:54:00

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""
+This module contais the sum_mixed_list method using typing annotaions
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    make_multiplier method
+    """
+    def mul(numFloat: float) -> float:
+        """
+        the mul method
+        """
+        return numFloat * multiplier
+    return mul
