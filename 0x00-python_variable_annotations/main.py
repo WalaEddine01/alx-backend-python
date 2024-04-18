5dfa2b6cc04307c0cab6221ffd6820451678fa02
walaeddine01 <walaaeddine33@gmail.com>
2024-04-18 19:30:30 +0200

    I made the changes on 2024-04-18 19:30:30

--- 0x00-python_variable_annotations/main.py
+++ 0x00-python_variable_annotations/main.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
-a = __import__('4-define_variables').a
-print("a is a {} with a value of {}".format(type(a), a))
+
+sum_list = __import__('5-sum_list').sum_list
+
+floats = [3.14, 1.11, 2.22]
+floats_sum = sum_list(floats)
+print(floats_sum == sum(floats))
+print(sum_list.__annotations__)
+print("sum_list(floats) returns {} which is a {}".format(floats_sum, type(floats_sum)))
