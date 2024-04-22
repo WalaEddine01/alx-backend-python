8ca860f3bc53ebfb42646209cb08d2ba93e6e442
walaeddine01 <walaaeddine33@gmail.com>
2024-04-22 22:42:57 +0200

    I made the changes on 2024-04-22 22:42:57

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+"""
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n, max_delay):
+    list_ = []
+    for i in range(n):
+        list_.append(await wait_random(max_delay))
+
+    return list_
