7fc3480ccc806d194485acc5f32fa3c531b1ccd3
walaeddine01 <walaaeddine33@gmail.com>
2024-04-22 23:14:38 +0200

    I made the changes on 2024-04-22 23:14:38

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -2,6 +2,17 @@
 '''
 This module contains method called measure_time
 '''
-wait_n = __import__("1-concurrent_coroutines.py").wait_n
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    this is the measure_time function
+    """
+    total_time = 0
+    delay = asyncio.run(wait_n(n, max_delay))
+    for i in delay:
+        total_time += i
+
+    return total_time / n
