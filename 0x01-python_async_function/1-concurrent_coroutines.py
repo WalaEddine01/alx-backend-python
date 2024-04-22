fffeede3a43f3c1354f7b9db4a9fa5ea4b8de1e0
walaeddine01 <walaaeddine33@gmail.com>
2024-04-22 23:01:31 +0200

    I made the changes on 2024-04-22 23:01:31

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,8 +2,8 @@
 """
 This module contains the wait_n method
 """
-wait_random = __import__('0-basic_async_syntax').wait_random
 from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
