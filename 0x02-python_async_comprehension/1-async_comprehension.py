90cd8ba56f5d1ee4db9e94873e9b126cd0539c0c
walaeddine01 <walaaeddine33@gmail.com>
2024-04-23 19:26:49 +0200

    I made the changes on 2024-04-23 19:26:49

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 """
-This Module contains the async_comprehension coroutine 
+This Module contains the async_comprehension coroutine
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import List
+async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_comprehension() -> AsyncGenerator:
-    
+async def async_comprehension() -> List[float]:
+    result = []
+    for i in async_generator():
+        result.append(i)
+
+    return result
