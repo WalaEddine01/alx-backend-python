316044d314aa5f31a26d5597fc974861e93ca67b
walaeddine01 <walaaeddine33@gmail.com>
2024-04-23 18:42:18 +0200

    I made the changes on 2024-04-23 18:42:18

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 """
+This Module contains the async_generator coroutine 
 """
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    the async_generator coroutine
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
