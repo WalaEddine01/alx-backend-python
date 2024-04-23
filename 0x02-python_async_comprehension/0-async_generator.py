202b96f9705c286911ef2e286be30dbded605dc6
walaeddine01 <walaaeddine33@gmail.com>
2024-04-23 19:02:16 +0200

    I made the changes on 2024-04-23 19:02:16

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ This Module contains the async_generator coroutine
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     the async_generator coroutine
     """
