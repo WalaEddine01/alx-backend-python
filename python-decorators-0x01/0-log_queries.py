d6fe849030f8683985eaf5b3ccb8f23dc5ae97e1
WalaEddine01 <walaaeddine33@gmail.com>
2025-05-23 22:49:18 +0100

    Made changes on 22:49:18

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module I am applying decorators concept
+on logging Data base Queries
+"""
+import sqlite3
+import functools
+
+#### decorator to lof SQL queries
+
+def log_queries(func):
+    def wrapper(*args, **kwargs):
+        yield kwargs
+        func(**kwargs)
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
