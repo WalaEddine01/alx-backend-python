bca22ff1379d9047a8a2d06265f19b5c66698763
WalaEddine01 <walaaeddine33@gmail.com>
2025-06-01 21:13:04 +0100

    Made changes on 21:13:04

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -27,6 +27,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
 
 # Application definition
 
