0184349791ba81dd67d24dd27d8a6c06d525f007
WalaEddine01 <walaaeddine33@gmail.com>
2025-06-01 21:11:27 +0100

    Made changes on 21:11:27

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'chats'
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -124,5 +125,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ALLOW_ALL_ORIGINS = True
