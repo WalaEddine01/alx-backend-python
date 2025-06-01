cb8b0e5796a64859291298e08f46bdf5c3980cbc
WalaEddine01 <walaaeddine33@gmail.com>
2025-06-01 21:43:13 +0100

    Made changes on 21:43:13

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -5,11 +5,18 @@ from .models import Conversation, Message
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.CharField()
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    phone_number = serializers.CharField()
+
     class Meta:
         model = User
         fields = ['id', 'email', 'first_name', 'last_name', 'phone_number']
 
 class MessageSerializer(serializers.ModelSerializer):
+    message_body = serializers.CharField()
+    sent_at = serializers.DateTimeField()
     sender = UserSerializer(read_only=True)
 
     class Meta:
@@ -18,8 +25,17 @@ class MessageSerializer(serializers.ModelSerializer):
 
 class ConversationSerializer(serializers.ModelSerializer):
     users = UserSerializer(many=True, read_only=True)
-    messages = MessageSerializer(many=True, read_only=True)
+    messages = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
         fields = ['conversation_id', 'users', 'messages']
+
+    def get_messages(self, obj):
+        messages = obj.messages.all().order_by('sent_at')
+        return MessageSerializer(messages, many=True).data
+
+    def validate_users(self, value):
+        if not value or len(value) < 2:
+            raise serializers.ValidationError("A conversation must include at least two users.")
+        return value
