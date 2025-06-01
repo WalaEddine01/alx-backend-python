69a7e587ff23e96c1936b7efbadb53b991ec3065
WalaEddine01 <walaaeddine33@gmail.com>
2025-06-01 21:50:07 +0100

    Made changes on 21:50:07

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,16 +1,11 @@
-from .views import HelloAPIView
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
-router = routers.DefaultRouter()
+router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
     path('', include(router.urls)),
-]
-
-urlpatterns = [
-    path('hello/', HelloAPIView.as_view()),
 ]
