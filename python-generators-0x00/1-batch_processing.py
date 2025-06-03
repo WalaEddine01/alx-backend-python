86cb6c8942420b1a3018c1c5372bebc3d38e08b6
WalaEddine01 <walaaeddine33@gmail.com>
2025-06-03 15:13:30 +0100

    Made changes on 15:13:30

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,50 +1,60 @@
 #!/usr/bin/python3
+from mysql import connector
+from os import getenv
+from dotenv import load_dotenv
+from itertools import islice
+load_dotenv()
+import sys
 
-import mysql.connector
-from typing import Iterator, List, Dict
 
-def connect_db() -> mysql.connector.connection.MySQLConnection:
-    """Establish database connection."""
-    return mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='',
-        database='ALX_prodev'
+def stream_users_in_batches(batch_size):
+    """
+     uses a generator to fetch rows one by one from the user_data table.
+     must use the Yield python generator.
+    """
+    connection = connector.connect(
+        host=getenv("MYSQL_HOST"),
+        port=getenv("MYSQL_PORT"),
+        user=getenv("MYSQL_USER"),
+        password=getenv("MYSQL_PASSWORD"),
+        database=getenv("MYSQL_DB")
     )
+    cursor = connection.cursor()
+    users = cursor.execute('SELECT * from user_data LIMIT %s', (batch_size, ))
+    users = cursor.fetchall()
+    for i in range(batch_size):
+        user = {'user_id': users[i][0], 'name': users[i][1],
+                'email': users[i][2], 'age': users[i][3]}
 
-def stream_users_in_batches(batch_size: int) -> Iterator[List[Dict]]:
+        #print(user)
+        yield user
+    cursor.close()
+
+def batch_processing(batch_size):
     """
-    Generator function that yields batches of users from the database.
-    
-    Args:
-        batch_size: Number of records to include in each batch
-        
-    Yields:
-        List[Dict]: List of user dictionaries
+    that processes each batch to filter users over the age of25
     """
-    cnx = connect_db()
-    cursor = cnx.cursor(dictionary=True)
-    
+    connection = connector.connect(
+        host=getenv("MYSQL_HOST"),
+        port=getenv("MYSQL_PORT"),
+        user=getenv("MYSQL_USER"),
+        password=getenv("MYSQL_PASSWORD"),
+        database=getenv("MYSQL_DB")
+    )
+    cursor = connection.cursor()
+    users = cursor.execute('SELECT * from user_data WHERE age > 25 LIMIT %s', (batch_size, ))
+    users = cursor.fetchall()
+    for i in range(batch_size):
+        user = {'user_id': users[i][0], 'name': users[i][1],
+                'email': users[i][2], 'age': users[i][3]}
+
+        #print(user)
+        yield user
+    cursor.close()
+
+
+if __name__ == "__main__":
     try:
-        cursor.execute("SELECT * FROM user_data")
-        while True:
-            batch = cursor.fetchmany(batch_size)
-            if not batch:
-                break
-            yield batch
-    finally:
-        cursor.close()
-        cnx.close()
-
-def batch_processing(batch_size: int) -> None:
-    """
-    Processes users in batches, filtering for those over 25 years old.
-    
-    Args:
-        batch_size: Size of each batch to process
-    """
-    for batch in stream_users_in_batches(batch_size):
-        # Single pass through the batch to filter users
-        for user in batch:
-            if user['age'] > 25:
-                print(user)
+        stream_users_in_batches(50)
+    except BrokenPipeError:
+        sys.stderr.close()
