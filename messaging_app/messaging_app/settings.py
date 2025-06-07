97bcaff2360ed4002d63cedd38cbe0d8faed6926
WalaEddine01 <walaaeddine33@gmail.com>
2025-06-07 17:13:19 +0100

    Made changes on 17:13:19

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -62,7 +62,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
+    'rest_framework.authentication.BasicAuthentication',
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
