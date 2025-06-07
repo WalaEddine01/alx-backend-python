3493e1e8aa4e1e494c2db9a946227b925d426022
WalaEddine01 <walaaeddine33@gmail.com>
2025-06-07 17:22:11 +0100

    Made changes on 17:22:11

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from rest_framework_nested import routers
 from .views import ConversationViewSet, MessageViewSet
+from .views import CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
@@ -10,6 +12,10 @@ conversations_router = routers.NestedDefaultRouter(router, r'conversations', loo
 conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
+
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('', include(router.urls)),
     path('', include(conversations_router.urls)),
 ]
