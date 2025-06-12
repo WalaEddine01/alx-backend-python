eab562adcad2763b951f9b1106de834ce4e5d86c
WalaEddine01 <walaaeddine33@gmail.com>
2025-06-12 05:46:24 +0100

    Made changes on 05:46:24

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+"""
+from mysql import connector
+from os import getenv
+from dotenv import load_dotenv
+
+
+class DatabaseConnection():
+    """
+    """
+    def __init__(self, user, password, database, host, port):
+        self.user = user
+        self.password = password
+        self.database = database
+        self.host = host
+        self.port = port
+    
+    def __enter__(self):
+        self.db_connection = connector.connect(
+            host=self.host,
+            port=self.port,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+        return self.db_connection
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.db_connection.close()
+
+if __name__ == '__main__':
+    load_dotenv()
+    host=getenv("MYSQL_HOST")
+    port=getenv("MYSQL_PORT")
+    user=getenv("MYSQL_USER")
+    password=getenv("MYSQL_PASSWORD")
+    database=getenv("MYSQL_DB")
+    
+    print(type(port))
+    with DatabaseConnection(user, password, database, host, port) as db:
+        print('db connected')
