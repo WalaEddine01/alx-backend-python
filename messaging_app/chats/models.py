91d7152fdc1dea2268ec5469f15d7dd99b59cafe
WalaEddine01 <walaaeddine33@gmail.com>
2025-06-17 21:31:41 +0100

    Made changes on 21:31:41

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,9 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from uuid import uuid4
+
+
+class User(models.Model):
+    '''
+    '''
+    user_id = models.UUIDField(primary_key=True, default=uuid4(), editable=False)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
+
 
 class Conversation(models.Model):
     """
     """
+    id = uuid4()
     name = models.CharField(max_length=100)
     users = models.ManyToManyField(User, related_name='converstaion')
     created_at = models.DateTimeField(auto_now_add=True)
