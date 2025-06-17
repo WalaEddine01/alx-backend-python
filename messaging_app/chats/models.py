ff86a6ae1f1d70459c6be628df7f93eb7d9c97e0
WalaEddine01 <walaaeddine33@gmail.com>
2025-06-17 21:33:52 +0100

    Made changes on 21:33:52

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -11,6 +11,7 @@ class User(models.Model):
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.email})"
