c58136e3f0798d3ea92270baf5b0ae605ebe8f0c
WalaEddine01 <walaaeddine33@gmail.com>
2025-06-18 12:09:26 +0100

    Made changes on 12:09:26

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -23,7 +23,6 @@ class Conversation(models.Model):
     conversation_id = models.UUIDField(primary_key=True, default=uuid4(), editable=False)
     name = models.CharField(max_length=100)
     users = models.ManyToManyField(User, related_name='converstaion')
-    participants = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -33,13 +32,15 @@ class Message(models.Model):
     """
     """
     message_id = models.UUIDField(primary_key=True, default=uuid4(), editable=False)
-    Conversation = sender = models.ForeignKey(User, on_delete=models.CASCADE)
-    models.ForeignKey(Conversation, on_delete=models.CASCADE)
-    message_body = models.TextField()
     sender = models.ForeignKey(User, on_delete=models.CASCADE)
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE)
+    Conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    message_body = models.TextField(default="")
+    sender = models.ForeignKey(User, related_name="sent_messages", on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name="received_messages", on_delete=models.CASCADE,
+                                 default=None, null=True, blank=True)
     content = models.TextField()
-    sent_at = models.DateTimeField(auto_now_add=True)
+    sent_at = models.DateTimeField(auto_now_add=True, verbose_name="Sent At",
+                                   null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
