347661ad1e361560c828e68917c218b3e8e45bf6
WalaEddine01 <walaaeddine33@gmail.com>
2025-06-18 14:50:09 +0100

    Made changes on 14:50:09

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters, status
+from rest_framework.response import Response
 from .models import User, Conversation, Message
 from .serializers import UserSerializer, ConversationSerializer, MessageSerializer
 from rest_framework.permissions import IsAuthenticated
@@ -12,7 +13,8 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    filter_backends = ["first_name", "last_name", "email"]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['first_name', 'last_name', 'email']
 
     def get_queryset(self):
         return self.queryset.order_by('first_name')
@@ -24,10 +26,13 @@ class ConversationViewSet(viewsets.ModelViewSet):
     """
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    filter_backends = [filters.SearchFilter]
     search_fields = ['name']
 
-    def get_queryset(self):
-        return self.queryset.order_by('name')
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
 class MessageViewSet(viewsets.ModelViewSet):
     """
@@ -36,7 +41,10 @@ class MessageViewSet(viewsets.ModelViewSet):
     """
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    filter_backends = [filters.SearchFilter]
     search_fields = ['content']
 
-    def get_queryset(self):
-        return self.queryset.order_by('sent_at')
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
